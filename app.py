a54f9df296620d8158c32efca6455c21e6bf9950
brianlong17-devgit config --global user.name brianlong17-devgit config --global user.name brianlong17-dev <brianlong17@gmail.com>
2026-01-12 17:54:55 +0000

    added love count animation

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, jsonify
 import random
+import os
 
 app = Flask(__name__)
+STATS_FILE = "stats.txt"
 
 @app.route('/')
 def home():
@@ -11,6 +13,44 @@ def home():
 def get_name():
     names = ["Professor Paws", "Lord Fluffington", "Sir Purr-a-lot", "Captain Meow", "General Whiskers"]
     return jsonify(name=random.choice(names))
+
+@app.route('/initial_pet')
+def initial_pet():
+    # 1. Read the current count from the file
+    if os.path.exists(STATS_FILE):
+        with open(STATS_FILE, "r") as f:
+            try:
+                count = int(f.read())
+            except ValueError:
+                count = 0
+    else:
+        count = 0
+
+    return jsonify(total_pets=count)
+
+
+@app.route('/pet_cat')
+def pet_cat():
+
+    # 1. Read the current count from the file
+    if os.path.exists(STATS_FILE):
+        with open(STATS_FILE, "r") as f:
+            try:
+                count = int(f.read())
+            except ValueError:
+                count = 0
+    else:
+        count = 0
+    
+    # Increase it
+    count += 1
+    
+    # Save it back
+    with open("stats.txt", "w") as f:
+        f.write(str(count))
+        
+    return jsonify(total_pets=count)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
